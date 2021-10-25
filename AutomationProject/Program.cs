9fadcb5b1d4c81cd61953bed74e46dfcbc275f5e
11035905 <11035905@H5CG1220K29>
2021-10-25 18:49:44 +0530

    Initial Commit

---
+++ AutomationProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace AutomationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IWebDriver driver = new ChromeDriver(@"C:\"))
+            {
+                string url = "http://localhost:57854";
+                driver.Navigate().GoToUrl(url);
+                Thread.Sleep(5000);
+
+                IWebElement element = driver.FindElement(By.Id("1"));
+                element.Click();
+                Thread.Sleep(3000);
+
+                IWebElement button = driver.FindElement(By.Id("proceed"));
+                button.Click();
+
+                Thread.Sleep(5000);
+                driver.Quit();
+            }
+        }
+    }
+}
